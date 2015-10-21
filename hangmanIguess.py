9ff072c45b374fa27d8dff5024dca7225d23ca0e
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-10-21 10:46:15 -0400

    Current Code

---
+++ hangmanIguess.py
@@ -0,0 +1,59 @@
+def get_word_from_list(dificultylevel,wordvalue):
+    '''retrieves word from list, given requested dificulty level and word to choose'''
+    pass
+
+def set_lives():
+    '''sets the number of incorrect guesses a player can make before a game over'''
+    pass
+
+def remove_life():
+     '''updates lives remaining after a incorrect guess was made'''
+     pass
+
+def player_life():
+    ''
+    pass
+
+def check_guess(guess):
+    '''checks if letter is in the selected word, and returns true or false.'''
+    pass
+
+def update_word():
+    pass
+
+def do_victory_thing():
+    pass
+
+def display_lives (lives):
+    print 'You have ',lives,'lives'
+    if lives==1:
+		print 'Ah snap dood, you only got ', lives, 'life left!'
+
+def get_player_guess (): pass
+
+def main ():
+    lives = 10
+    wordlist = ['foo','bar']
+    word = get_word_from_list(wordlist,7)
+    while lives:
+        display_lives(lives)
+        guess = get_player_guess()
+        right = check_guess(guess)
+	if right: 
+            victory = update_word(word, guess)
+            if victory:
+                do_victory_thing()
+                return
+        else:
+            lives = lives - 1
+	if lives==0:
+		print 'game over man, game over.'
+		
+
+def test_get_word ():
+    get_word_from_list(['foo','bar','baz'],1)
+    get_word_from_list(['foo','bar','baz'],2)
+    get_word_from_list(['foo','bar','baz'],3)
+    get_word_from_list(['foo','bar','baz'],4)
+
+main()
