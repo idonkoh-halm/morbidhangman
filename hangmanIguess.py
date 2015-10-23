222e8b47a0be80891e71893c7bc2bb1ecb74186e
jackpanneton <programmer@hs-lab-2.local>
2015-10-23 08:59:43 -0400

    removing lives as a function

--- hangmanIguess.py
+++ hangmanIguess.py
@@ -2,17 +2,16 @@ def get_word_from_list(dificultylevel,wordvalue):
     '''retrieves word from list, given requested dificulty level and word to choose'''
     pass
 
-def set_lives(x):
-    '''sets the number of incorrect guesses a player can make before a game over'''
-
 
 def player_life():
     '''
     pass
+    '''
 
 def check_guess(guess):
-    '''#checks if letter is in the selected word, and returns true or false.'''
+    '''
     pass
+    '''
 
 def update_word():
     pass
@@ -20,19 +19,19 @@ def update_word():
 def do_victory_thing():
     pass
 
-def display_lives (set_lives):
-    print 'You have ',set_lives,'lives'
-    if set_lives==1:
-		print 'Ah snap dood, you only got ', set_lives, 'life left!'
+def display_lives (lives):
+    print 'You have ',lives,'lives'
+    if lives==1:
+		print 'Ah snap dood, you only got ', lives, 'life left!'
 
 def get_player_guess (): pass
 
 def main ():
-    set_lives(10)
+    lives=10
     wordlist = ['foo','bar']
     word = get_word_from_list(wordlist,7)
-    while set_lives:
-        display_lives(set_lives)
+    while lives:
+        display_lives(lives)
         guess = get_player_guess()
         right = check_guess(guess)
 	if right: 
@@ -41,8 +40,8 @@ def main ():
                 do_victory_thing()
                 return
         else:
-            set_lives = set_lives - 1
-	if set_lives==0:
+            lives = lives - 1
+	if lives==0:
 		print 'game over man, game over.'
 		
 
