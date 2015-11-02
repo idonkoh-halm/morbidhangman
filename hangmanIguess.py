0a722c57854614563c91cf6607c236e4f59e6ecb
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2015-11-02 13:02:23 -0500

    Returned word value as a variable

--- hangmanIguess.py
+++ hangmanIguess.py
@@ -1,10 +1,12 @@
 def get_word_from_list():
-
-    '''retrieves word from a list following a player chosen number that represents a word from that list.'''
+#Get word from list function made by Isaac.
+    '''Retrieves word from a list following a player chosen number that represents a word from that list. Will also generate blanks in place of letters.'''
     a = ['Mario','Link','Pikachu','Donkey Kong', 'Samus', 'Fox', 'Yoshi', 'Kirby','Luigi','Captain Falcon', 'Ness', 'Jigglypuff']
     wordvalue=int(raw_input("Pick a number between 0 and 11."))
-    for i in range (1):
-            print a[wordvalue]        
+    #print a[wordvalue]
+    return a[wordvalue]
+
+    
 
     
 def check_guess (word, guess):
@@ -45,6 +47,7 @@ def get_player_guess ():
 
 def main ():
     lives=10
+    word=get_word_from_list()
     while lives:
         display_lives(lives)
         guess = get_player_guess()
@@ -64,8 +67,7 @@ def main ():
 def test_get_word ():
     for i in range (12):
         get_word_from_list()
-
-
+        
 def test_check_guess ():
     if check_guess('python','a')==False:
         print 'Success'
@@ -83,6 +85,3 @@ def test_check_guess ():
 
 
 test_get_word()
-
-
-
