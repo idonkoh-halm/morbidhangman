5895fd208b2efceddbf3053bd32ea69aafd301ad
Isaac Donkoh-Halm <isaac.donkoh-halm@innovationcharter.org>
2015-12-09 18:26:05 -0500

    now its a game!

--- hangmanIguess.py
+++ hangmanIguess.py
@@ -3,8 +3,8 @@ import random
 def get_word_from_list():
 #Get word from list function made by Isaac.
     '''Retrieves word from a list following a player chosen number that represents a word from that list.'''
-    a = ['Mario','Link','Pikachu','Donkey Kong', 'Samus', 'Fox', 'Yoshi', 'Kirby','Luigi','Captain Falcon', 'Ness', 'Jigglypuff']
-    wordvalue=int(raw_input("Pick a number between 0 and 11."))
+    a = ['formulation','guilty','hoofs','gun', 'finch', 'wonder', 'villain', 'amber','jazz','blooper', 'dizzy', 'germ', 'clod', 'rib', 'rambunctious','aggressive','askew','banjo','blitz', 'daiquiri','jazzy','zombie','difficult','jinx','rhythm','vat']
+    wordvalue=int(raw_input("Pick a number between 0 and 25."))
     #print a[wordvalue]
     return a[wordvalue]
 
@@ -55,10 +55,10 @@ def blankwords(word,guess):
         
         
 def main ():
-    response_list = ['bad times ahead.','bad tem ahead']
-    response_list_dark = ['']
-    response_list_darker = ['']
-    response_list_yetdarker = ['']
+    response_list = ['Oops Looks like you got the wrong answer. Time to bring out the malfested!','Uh oh! That is not it! Do not worry traveler, it is only the guilty who need to worry now',]
+    response_list_dark = ['Yes! Yes! Well, you got the answer wrong, but now we will be closer to ending the heretic!','*You begin to study the face of the prisoner. Looks as if he has not eaten in weeks. Dirt and some gashes of blood of the stoning are prevealent over his malnourished body.']
+    response_list_darker = ['*The crowd begins to chant: The Greater Good.','The prisoner seems to have stopped struggling from before. As he passes each guard, stray rocks and glass are thrown at him.','*Out of the blood thirsty village, a small pocket of people look on with horror. A woman, maybe the his wife, covers the eyes of a newborn infant. The woman looks about 20.' ]
+    response_list_yetdarker = ['The Greater Good.','The Greater Good.', 'The Greater Good.', 'The Greater Good.', 'The Greater Good.', 'The Greater Good.','The Greater Good.','He does not look like he is having a good time.']
     random.shuffle(response_list)
     random.shuffle(response_list_dark)
     random.shuffle(response_list_darker)
@@ -89,8 +89,16 @@ def main ():
                 print 'You monster.'
             elif lives==9:
                 print response_list.pop()
-                
-            
+            elif lives==6:
+                print response_list_dark.pop()
+            elif lives==5:
+                print response_list_darker.pop()
+            elif lives==4:
+                print response_list_yetdarker.pop()
+            elif lives==3:
+                print response_list_yetdarker.pop()
+            elif lives==2:
+                print response_list_yetdarker.pop()
 def test_check_guess ():
     if check_guess('python','a')==False:
         print 'Success'
@@ -119,14 +127,32 @@ def test_check_guess ():
     else:
         print 'Failed with capital letter'
             
-def test_is_complete():
-    print 'true?', is_complete(set(list('Mario')), 'Mario')
-    print 'false?', is_complete(set(list('Mario')), 'Luigi')
-    print 'true?', is_complete(set(list('Link')), 'Link')
-    print 'false?', is_complete(set(list('Link')), 'Zelda')
-
-#get_player_guess()
-#test_is_complete()
-#test_check_guess() 
+ 
+print "Welcome to hangman! Pick a number 1 through 25"
+time.sleep(5)
+print "..."
+time.sleep(5)
+print "Not yet. Sorry about that."
+time.sleep(4)
+print "I'll explain the rules first."
+time.sleep(2)
+print "When you're guessing, no capital letters are allowed, and there are no spaces. You cannot guess the whole word at once."
+time.sleep(3)
+print "One letter at a time please."
+time.sleep(4)
+print "Trust me."
+time.sleep(3)
+print "ok, now you can start. Have fun!"
 main()
-time.sleep(60)
+time.sleep(6)
+print "Hey can you do me a favor?"
+time.sleep(7)
+print "Can you press that squiggly button and K at the same time? It would really help me out. I'll wait." 
+time.sleep(8)
+print "Thanks."
+time.sleep(4)
+print "Also, thank you for playing my game. This was written by Isaac Donkoh-Halm and Devin Cook."
+time.sleep(5)
+print "Good night!"
+time.sleep(9)
+print ";)"
